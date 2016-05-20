6357795c232ad6a662802ee5cdfa26debfc1f5ef
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 19:09:09 -0700

    Pass 12 tests on refactor

--- app/models/link.js
+++ app/models/link.js
@@ -1,4 +1,5 @@
 var mongoose = require('../config');
+var crypto = require('crypto');
 
 var schema = mongoose.Schema({
   url: String,
@@ -8,6 +9,13 @@ var schema = mongoose.Schema({
   visits: Number
 });
 
+schema.pre('save', function(next) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+
+  next();
+});
 
 var Link = mongoose.model('Link', schema);
 
--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,25 @@
 var mongoose = require('../config');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
 
 var schema = mongoose.Schema({
   username: String,
   password: String
+});
+
+schema.method('comparePassword', function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+});
+
+schema.method('hashPassword', function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+    });
 });
 
 var User = mongoose.model('User', schema);
